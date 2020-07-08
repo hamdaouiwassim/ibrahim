31d032c731daa5429b25c9db808e6288ff9a55f8
hamdaouiwassim <hamdaouiwassim@gmail.com>
2020-07-08 15:33:01 +0200

    before last session

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,12 +19,12 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
+<body style="min-height:100vh;background-size:cover;background-position:center;background-image: url('{{ asset('images/login.jpg') }}')">
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    HealthFirst
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
--- resources/views/patients/home.blade.php
+++ resources/views/patients/home.blade.php
@@ -1,19 +1,103 @@
-@extends('layouts.pat')
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-<div class="container">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body style="min-height:100vh;background-size:cover;background-image: url('{{ asset('images/coverSearch.jpg') }}')">
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    Centres Medicaux
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item ">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item mr-3">
+                                <a href="{{ route('home')}}" class="btn btn-primary"> Chercher Centre </a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+
+                                    <a href="{{ route('patientsCentres') }}" class="dropdown-item">Centres</a>
+                                    <a href="{{ route('patientsReservations') }}" class="dropdown-item">Reservations</a>
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+     
+
+
+<div class="col-12"  >
     <div class="row justify-content-center">
-        <div class="col-md-12">
+        <div class="col-md-12 pt-5">
             
         <form class="row pt-5" action="{{ route('cherchercentre')}}" method="post">
             @csrf
-                            <div class="form-group col-3">
+            <div class="row pt-5 col-12">
+
+            
+                            <div class="form-group col-3 pt-5">
                                 <input type="text" name="ville" class="form-control" placeholder="Entrer une ville de centre ..." >
                             </div>
-                            <div class="form-group col-3">
+                            <div class="form-group col-3 pt-5">
                                 <input type="text" name="region" class="form-control" placeholder="Entrer une region...">
                             </div>
-                            <div class="form-group col-3">
+                            <div class="form-group col-3 pt-5">
                             <select class="form-control" name="type" class="form-control">
                                 <option value="">
                                     Type centre ...
@@ -31,14 +115,17 @@
                             </select>
                         </div>
                         
-                        <div class="form-group col-3">
+                        <div class="form-group col-3 pt-5">
                             <input type="submit" value="Chercher" class="btn btn-success">
                         </div>
 
                   </form>
-               
+                </div> 
             </div>
         </div>
     </div>
 </div>
-@endsection
+
+</div>
+</body>
+</html>
